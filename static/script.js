2214e7a7b7b98dd48e6169643e7df888a85aaa9b
sandeep780049 <anujsandeepyadav@gmail.com>
2025-08-09 00:56:06 +0530

    Update script.js

--- static/script.js
+++ static/script.js
@@ -1,1 +1,19 @@
+// Copy text function
+function copyToClipboard(elementId) {
+    let text = document.getElementById(elementId).innerText;
+    navigator.clipboard.writeText(text).then(() => {
+        alert("Copied to clipboard!");
+    });
+}
 
+// Download text function
+function downloadText(filename, elementId) {
+    let text = document.getElementById(elementId).innerText;
+    let blob = new Blob([text], { type: "text/plain" });
+    let link = document.createElement("a");
+    link.href = URL.createObjectURL(blob);
+    link.download = filename;
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+}
